b464fc4ef36afd491a68cada951379c75384ef45
Frederik la Cour <frediklac@gmail.com>
2019-04-10 20:43:07 +0200

    trail n error

--- sketch.js
+++ sketch.js
@@ -1,12 +1,35 @@
 var video;
+var button;
+var snapshots = {
+  array: [],
+  width: 320,
+  height: 240,
+};
+
+var xdim = 800;
+var ydim = 240;
 
 function setup() {
-  //createCanvas(displayWidth, displayHeight);
-  //background(51);
+  createCanvas(800, 240);
+  background(51);
   video = createCapture(VIDEO);
-  video.size(displayWidth,displayHeight);
+  video.size(320, 240);
+  //video.hide();
+
+  button = createButton('snap');
+  button.mousePressed(takesnap);
 }
 
-function draw() {
+function takesnap() {
+  //snapshots.push(video.get())
+  image(video, 0, 0);
+}
 
+
+function draw() {
+  for (var i = 0; i < snapshots.length; i++) {
+    image(snapshots[i], 320, 240);
+  }
+
+//image(video, 0, 0);
 }
